4cb4d8eaa45d838e03f59f1e3a31ecb7e9b45e05
victorhabigzang <victor.habigzang@escola.pr.gov.br>
2026-09-14 10:55:48 -0300

    ghjklç

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criarCartao(
+    `Geografia`,
+    `Qual é a capital do Paraná? ` ,
+    'Curitiba'
+
+)
+
+criarCartao(
+    `Geografia`,
+    `Qual é a capital do Paraná? ` ,
+    'Curitiba'
+    )
+
+    criarCartao(
+    `Geografia`,
+    `Qual é a capital do Paraná? ` ,
+    'Curitiba' 
+    )
+
+    criarCartao(
+    `Geografia`,
+    `Qual é a capital do Paraná? ` ,
+    'Curitiba' 
+    )
